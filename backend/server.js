3c7b837e13152f452851518b173e0c626dbef4f7
Ahmed Adel <ahmed45adel@yahoo.com>
2025-07-12 07:10:28 +0300

    Initialize backend with basic auth routes

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//auth
+app.use('/api/auth', authRoutes)
+
+
+app.listen(5000, ()=> console.log("server is running on http://localhost:"+PORT))
