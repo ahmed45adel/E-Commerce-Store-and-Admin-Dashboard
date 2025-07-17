19e3917e86a2d32b8461ee9e2b81874ccbb1abb1
Ahmed Adel <ahmed45adel@yahoo.com>
2025-07-17 22:53:27 +0300

    update url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "https://store-mu-ebon.vercel.app/api",
+	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "https://e-commerce-store-and-admin-dashboar.vercel.app/api",
 	withCredentials: true, // send cookies to the server
 });
 
